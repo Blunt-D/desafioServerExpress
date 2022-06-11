21b3f2e3991216b789210f5333c6b08ed258a052
Felipe Villarrubia <felipevillarrubiasymmes@gmail.com>
2022-06-11 14:21:21 -0400

    Se aplican funcionciones asincrónicas al ejercicio

--- server.js
+++ server.js
@@ -3,31 +3,46 @@ import express from "express"
 const app = express()
 const port = 8080
 
-const read = () => {
-    const data = fs.readFileSync('./productos.json', "utf-8");
-    return JSON.parse(data);
-};
+app.listen(port, () => {
+    {
+        console.log(`Servidor http escuchando en el puerto ${port}`);
+    }
+})
+
+const read = async () => {
+    try {
+        const data = await fs.promises.readFile('./productos.json', "utf-8");
+        return JSON.parse(data);
+    } catch (error) {
+        res.send({'Error': e.message})
+    }
+
+    }
 
 
 app.get('/', (req, res) => {
     res.send("<h1>Bienvenidos al tercer desafío</h1>")
 })
 
-app.get('/productos', (req, res) => {
-    const allProducts =  read()
-    const showAllProducts =  JSON.stringify(allProducts.sort())
-    res.send(`Los productos disponibles son ${showAllProducts}`)
+app.get('/productos', async (req, res) => {
+    try {
+        const allProducts = await read()
+        const showAllProducts = JSON.stringify(allProducts)
+        res.send(`Los productos disponibles son ${showAllProducts}`)
+    } catch (error) {
+        res.send({'Error': error.message})
+    }
+
 })
 
-app.get('/productoRandom', (req, res) => {
-    const allProducts =  read()
-    const randomizer = Math.floor(Math.random()*allProducts.length);
-    const randomProduct =  JSON.stringify(allProducts[randomizer])
-    res.send(`${randomProduct}`)
-})
-
-app.listen(port, () => {
-    {
-        console.log(`Servidor http escuchando en el puerto ${port}`);
+app.get('/productoRandom', async (req, res) => {
+    try {
+        const allProducts =  await read()
+        const randomizer = Math.floor(Math.random()*allProducts.length);
+        const randomProduct =  JSON.stringify(allProducts[randomizer])
+        res.send(`${randomProduct}`)
+    } catch (error) {
+        res.send({'Error': e.message})
     }
 })
+
